021b3cad068418ee1b9207288ee0aef12c0a151a
kunchalarajesh <rajesh@acedezines.com>
2022-07-06 16:02:50 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,9 @@ function Home() {
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no" />
         <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+        <link rel="preconnect" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" crossorigin />
         <link rel="stylesheet" href="slick/slick.css" />
         <link rel="stylesheet" href="slick/slick-theme.css" />
-        <link rel="stylesheet" href="css/styles.css" />
         <title>Creative Agency</title>
       </Head>
       <Navbar />
