5e492eef9274f21f3119a7d2fa92252cc3cebc49
kunchalarajesh <rajesh@acedezines.com>
2022-07-06 14:00:08 +0530

    first commit

--- pages/About.js
+++ pages/About.js
@@ -15,7 +15,7 @@ function About(){
                 </div>
                 <div className="about-text">
                     <h2>OUR STORY</h2>
-                    <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus</p>
+                    <p>This is Photoshops version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus</p>
                     <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
                     <Link href="#" className="buttons">Learn more</Link>
                 </div>  
